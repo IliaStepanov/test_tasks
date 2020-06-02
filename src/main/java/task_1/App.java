58a8a83d725447ed79967047bf43cdb6cb875bec
Ilia_Stepanov <Ilia_Stepanov@epam.com>
2020-06-02 12:12:52 +0300

    Task 1

--- src/main/java/task_1/App.java
+++ src/main/java/task_1/App.java
@@ -1,12 +1,12 @@
 package main.java.task_1;
 
 public class App {
-    public static void main(String[] args) {
-        int[] array = new int[]{0, 0, 1, 1, 2, 3, 4, 4, 5, 5, 5, 44, 55, 9, 2, 1, 7, 7, 6, 4, 1, 2, 44, 2, 2, 2};
 
+    public static void main(String[] args) {
+
+        int[] array = new int[]{0, 0, 1, 1, 2, 3, 4, 4, 5, 5, 5, 44, 55, 9, 2, 1, 7, 7, 6, 4, 1, 2, 44, 2, 2, 2};
 
         ArrayEntryCounter arrayEntryCounter = new ArrayEntryCounter();
         System.out.println(arrayEntryCounter.count(array));
-
     }
 }
