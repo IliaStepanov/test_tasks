54fd3b12aa7c8cd0c95419ffdfe130c3c4e9f279
Ilia_Stepanov <Ilia_Stepanov@epam.com>
2020-06-03 10:27:11 +0300

    Task 3

---
+++ src/main/java/task_3/App.java
@@ -0,0 +1,15 @@
+package main.java.task_3;
+
+public class App {
+    public static void main(String[] args) {
+        long i1 = 1234560;
+        long i2 = 98797410;
+
+        System.out.printf("Before: %d, after: %d\n", i1, changeLastZeroToOne(i1));
+        System.out.printf("Before: %d, after: %d\n", i2, changeLastZeroToOne(i2));
+    }
+
+    public static long changeLastZeroToOne(long i) {
+        return i | (i + 1);
+    }
+}
